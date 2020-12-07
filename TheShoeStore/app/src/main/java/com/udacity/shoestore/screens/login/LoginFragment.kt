2d7df33f20e28d102a2e16b953712dbb6f923acd
doge-is-dope <maple3312936@outlook.com>
2020-12-07 23:27:11 +0800

    feat: add Welcome and its navigation

--- TheShoeStore/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ TheShoeStore/app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
@@ -13,19 +14,33 @@ import timber.log.Timber
 
 class LoginFragment : Fragment() {
 
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: FragmentLoginBinding
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding: FragmentLoginBinding =
+        binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        binding.loginViewModel = viewModel
+
         binding.btnLogin.setOnClickListener(
-            Navigation.createNavigateOnClickListener(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            Navigation.createNavigateOnClickListener(
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(viewModel.email.value.toString())
+            )
         )
 
         binding.btnCreate.setOnClickListener(
-            Navigation.createNavigateOnClickListener(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            Navigation.createNavigateOnClickListener(
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(
+                    viewModel.email.value.toString()
+                )
+            )
         )
         return binding.root
     }
---
+++ TheShoeStore/app/src/main/java/com/udacity/shoestore/screens/login/LoginViewModel.kt
@@ -0,0 +1,23 @@
+package com.udacity.shoestore.screens.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String>
+        get() = _email
+
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String>
+        get() = _password
+
+
+    init {
+        _email.value = "test@test.com"
+        _password.value = "123456"
+    }
+
+
+}
