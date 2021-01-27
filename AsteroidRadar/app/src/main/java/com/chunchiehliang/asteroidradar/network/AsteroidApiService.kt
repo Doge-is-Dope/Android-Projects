65ddfab48981f5a45819af7f15fd5b56aaae8491
doge-is-dope <maple3312936@outlook.com>
2021-01-28 00:07:22 +0800

    feat: use ScalarsConverter to get raw response

--- AsteroidRadar/app/src/main/java/com/chunchiehliang/asteroidradar/network/AsteroidApiService.kt
+++ AsteroidRadar/app/src/main/java/com/chunchiehliang/asteroidradar/network/AsteroidApiService.kt
@@ -4,8 +4,10 @@ import com.chunchiehliang.asteroidradar.domain.Asteroid
 import com.chunchiehliang.asteroidradar.utils.Constants.BASE_URL
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 import retrofit2.http.Query
@@ -15,18 +17,19 @@ private val moshi = Moshi.Builder()
     .build()
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addConverterFactory(ScalarsConverterFactory.create())
+
     .baseUrl(BASE_URL)
     .build()
 
 
 interface AsteroidApiService {
     @GET("neo/rest/v1/feed")
-    suspend fun getAsteroids(
+    fun getAsteroids(
         @Query("start_date") startDate: String,
         @Query("end_date") endDate: String,
-        @Query("api_key") key: String,
-    ): List<Asteroid>
+        @Query("api_key") apiKey: String,
+    ): Call<String>
 }
 
 
--- AsteroidRadar/app/src/main/java/com/chunchiehliang/asteroidradar/ui/main/MainViewModel.kt
+++ AsteroidRadar/app/src/main/java/com/chunchiehliang/asteroidradar/ui/main/MainViewModel.kt
@@ -5,8 +5,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chunchiehliang.asteroidradar.domain.Asteroid
+import com.chunchiehliang.asteroidradar.network.Network
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import timber.log.Timber
 
 enum class AsteroidApiStatus { LOADING, ERROR, DONE }
 
@@ -15,6 +20,10 @@ class MainViewModel : ViewModel() {
     private val _status = MutableLiveData<AsteroidApiStatus>()
     val status: LiveData<AsteroidApiStatus>
         get() = _status
+
+    private val _response = MutableLiveData<String>()
+    val response: LiveData<String>
+        get() = _response
 
     private val _asteroidList = MutableLiveData<List<Asteroid>>()
     val asteroidList: LiveData<List<Asteroid>>
@@ -25,24 +34,40 @@ class MainViewModel : ViewModel() {
         get() = _navigateToSelectedAsteroid
 
     init {
-        getAsteroidList()
+        // todo: this is for testing
+//        getAsteroidList()
+        getAsteroids()
     }
 
     private fun getAsteroidList() {
         viewModelScope.launch {
             _status.value = AsteroidApiStatus.LOADING
             try {
-                delay(2000)
-                _asteroidList.value =
-                    listOf(
-                        Asteroid(1, "abc", "2020-01-01", 23.1, 21.1, 1123.2, 50.6, false),
-                        Asteroid(2, "cde", "2020-01-02", 1.1, 124.1, 58.5, 44.1, true)
-                    )
+//                delay(2000)
+//                _asteroidList.value = Network.retrofitService.getAsteroids("2015-09-07", "2015-09-08", "DEMO_KEY")
                 _status.value = AsteroidApiStatus.DONE
             } catch (e: Exception) {
+                Timber.e("Error: $e")
                 _status.value = AsteroidApiStatus.ERROR
                 _asteroidList.value = ArrayList()
             }
+        }
+    }
+
+    private fun getAsteroids() {
+        _response.value = "Loading..."
+        viewModelScope.launch {
+
+            Network.retrofitService.getAsteroids("2015-09-07", "2015-09-08", "DEMO_KEY")
+                .enqueue(object : Callback<String> {
+                    override fun onFailure(call: Call<String>, t: Throwable) {
+                        _response.value = "Failure: " + t.message
+                    }
+
+                    override fun onResponse(call: Call<String>, response: Response<String>) {
+                        _response.value = response.body()
+                    }
+                })
         }
     }
 
