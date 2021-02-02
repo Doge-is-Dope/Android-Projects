b38700d1fa9e9f8e51b8664d53a09d95e4d41340
doge-is-dope <maple3312936@outlook.com>
2021-02-02 22:38:11 +0800

    feat: add worker to refresh data in database

--- AsteroidRadar/app/src/main/java/com/chunchiehliang/asteroidradar/AsteroidRadarApplication.kt
+++ AsteroidRadar/app/src/main/java/com/chunchiehliang/asteroidradar/AsteroidRadarApplication.kt
@@ -1,9 +1,14 @@
 package com.chunchiehliang.asteroidradar
 
 import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.chunchiehliang.asteroidradar.work.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class AsteroidRadarApplication : Application() {
 
@@ -15,5 +20,32 @@ class AsteroidRadarApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+
+        delayedInit()
+    }
+
+    private fun delayedInit() = applicationScope.launch {
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                setRequiresDeviceIdle(true)
+            }.build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
