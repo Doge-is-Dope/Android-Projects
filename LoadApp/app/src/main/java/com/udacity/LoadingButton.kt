8790cd76b985721221fce362fae6544ccc2810ad
doge-is-dope <maple3312936@outlook.com>
2021-02-15 22:06:14 +0800

    feat: create a custom button with two custom attributes

--- LoadApp/app/src/main/java/com/udacity/LoadingButton.kt
+++ LoadApp/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,10 +1,11 @@
 package com.udacity
 
-import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,21 +14,69 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
-    private val valueAnimator = ValueAnimator()
+    private var textColor = 0
+    private var buttonColor = 0
+
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+    }
+
 
     init {
+        isClickable = true
+
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            textColor = getColor(
+                R.styleable.LoadingButton_textColor, 0
+            )
+            buttonColor = getColor(R.styleable.LoadingButton_buttonColor, 0)
+        }
 
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
+        drawBackground(canvas)
+
+        drawText(canvas)
+    }
+
+
+    private fun drawBackground(canvas: Canvas) {
+        paint.color = buttonColor
+        canvas.drawRect(
+            0.0f,
+            0.0f,
+            widthSize.toFloat(),
+            heightSize.toFloat(),
+            paint
+        )
+    }
+
+
+    private fun drawText(canvas: Canvas) {
+        paint.apply {
+            color = textColor
+            textAlign = Paint.Align.CENTER
+            textSize = 24.0f * resources.displayMetrics.density
+        }
+        val metricsInt = paint.fontMetricsInt
+        val posX = widthSize / 2.0f
+        val textHeight = metricsInt.descent - metricsInt.ascent
+        val posY = (heightSize - textHeight) / 2.0f - metricsInt.ascent
+        canvas.drawText(
+            resources.getString(R.string.button_download),
+            posX,
+            posY,
+            paint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -41,6 +90,15 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+    }
+
+    override fun performClick(): Boolean {
+        // Enables accessibility events as well as calls onClickListener()
+        if (super.performClick()) return true
+
+        // Invalidates the entire view, forcing a call to onDraw() to redraw the view
+        invalidate()
+        return true
     }
 
 }
