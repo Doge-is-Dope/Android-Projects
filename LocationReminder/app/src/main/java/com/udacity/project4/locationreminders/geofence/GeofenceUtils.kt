07e6c92fdc5efe81d696e855a791c049d48f5feb
clement <chunchieh.liang@coolbitx.com>
2021-03-03 14:15:43 +0800

    refactor: add geofence in fragment

--- LocationReminder/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceUtils.kt
+++ LocationReminder/app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceUtils.kt
@@ -1,72 +1,52 @@
 package com.udacity.project4.locationreminders.geofence
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.IntentSender
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.geofence.GeofencingConstants.GEOFENCE_EXPIRATION_IN_MILLISECONDS
+import com.udacity.project4.locationreminders.geofence.GeofencingConstants.GEOFENCE_RADIUS_IN_METERS
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
-import com.udacity.project4.locationreminders.savereminder.SaveReminderFragment
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 object GeofenceUtils {
-
-    private const val GEOFENCE_RADIUS_IN_METERS = 100f
-    private val GEOFENCE_EXPIRATION_IN_MILLISECONDS: Long = TimeUnit.HOURS.toMillis(1)
-
     const val EXTRA_REMINDER = "REMINDER"
 
-
-    private fun getGeofencePendingIntent(
-        context: Context,
-        reminderDataItem: ReminderDataItem
-    ): PendingIntent {
-        val intent = Intent(context, GeofenceBroadcastReceiver::class.java)
-        intent.putExtra(EXTRA_REMINDER, reminderDataItem)
-        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
-
-    @SuppressLint("MissingPermission")
-    fun addGeofence(context: Context, reminderDataItem: ReminderDataItem) {
-
-        val geofence = Geofence.Builder()
-            .setRequestId(reminderDataItem.id)
-            .setCircularRegion(
-                reminderDataItem.latitude!!.toDouble(),
-                reminderDataItem.longitude!!.toDouble(),
-                GEOFENCE_RADIUS_IN_METERS
+    /**
+     * Returns the error string for a geofencing error code.
+     */
+    fun errorMessage(context: Context, errorCode: Int): String {
+        val resources = context.resources
+        return when (errorCode) {
+            GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE -> resources.getString(
+                R.string.geofence_not_available
             )
-            .setExpirationDuration(GEOFENCE_EXPIRATION_IN_MILLISECONDS)
-            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
-            .build()
-
-        val geofencingRequest = GeofencingRequest.Builder()
-            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
-            .addGeofence(geofence)
-            .build()
-
-        val pendingIntent = getGeofencePendingIntent(context, reminderDataItem)
-
-        val geofencingClient = LocationServices.getGeofencingClient(context)
-
-
-        geofencingClient.addGeofences(
-            geofencingRequest,
-            pendingIntent
-        )
-            .addOnSuccessListener {
-                Timber.d("Success! id: ${geofence.requestId}")
-            }
-            .addOnFailureListener {
-                Timber.e("Failure! id: ${context.getString(R.string.geofences_not_added)}")
-            }
+            GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES -> resources.getString(
+                R.string.geofence_too_many_geofences
+            )
+            GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS -> resources.getString(
+                R.string.geofence_too_many_pending_intents
+            )
+            else -> resources.getString(R.string.geofence_unknown_error)
+        }
     }
+}
 
+internal object GeofencingConstants {
 
+    /**
+     * Used to set an expiration time for a geofence. After this amount of time, Location services
+     * stops tracking the geofence. For this sample, geofences expire after one hour.
+     */
+    val GEOFENCE_EXPIRATION_IN_MILLISECONDS: Long = TimeUnit.HOURS.toMillis(1)
+
+    const val GEOFENCE_RADIUS_IN_METERS = 100f
+    internal const val ACTION_GEOFENCE_EVENT = "action.ACTION_GEOFENCE_EVENT"
+    const val EXTRA_GEOFENCE_INDEX = "GEOFENCE_INDEX"
 }
--- LocationReminder/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ LocationReminder/app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,22 +1,33 @@
 package com.udacity.project4.locationreminders.savereminder
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.annotation.TargetApi
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSaveReminderBinding
-import com.udacity.project4.locationreminders.geofence.GeofenceUtils
+import com.udacity.project4.locationreminders.geofence.GeofenceBroadcastReceiver
+import com.udacity.project4.locationreminders.geofence.GeofencingConstants
+import com.udacity.project4.locationreminders.geofence.GeofencingConstants.ACTION_GEOFENCE_EVENT
 import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class SaveReminderFragment : BaseFragment() {
     //Get the view model this time as a single to be shared with the another fragment
@@ -25,6 +36,14 @@ class SaveReminderFragment : BaseFragment() {
 
     private val runningQOrLater = android.os.Build.VERSION.SDK_INT >=
             android.os.Build.VERSION_CODES.Q
+
+    // A PendingIntent for the Broadcast Receiver that handles geofence transitions.
+    private val geofencePendingIntent: PendingIntent by lazy {
+        val intent = Intent(requireContext(), GeofenceBroadcastReceiver::class.java)
+        intent.action = ACTION_GEOFENCE_EVENT
+        PendingIntent.getBroadcast(requireContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,6 +62,7 @@ class SaveReminderFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = this
+
         binding.selectLocation.setOnClickListener {
             if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
                 navigateToSelectLocationFragment()
@@ -52,12 +72,16 @@ class SaveReminderFragment : BaseFragment() {
         }
 
         binding.saveReminder.setOnClickListener {
-            if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
-                checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
-            ) {
-                saveReminder()
+            val backgroundLocationPermission = Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            if (checkPermission(backgroundLocationPermission)) {
+                // the foreground location permission will be validated
+                saveReminderAndStartGeofence()
             } else {
-                requestForegroundAndBackgroundPermissions()
+                if (shouldShowRequestPermissionRationale(backgroundLocationPermission)) {
+                    showRationaleDialog()
+                } else {
+                    requestBackgroundPermissionForGeofence()
+                }
             }
         }
     }
@@ -75,15 +99,23 @@ class SaveReminderFragment : BaseFragment() {
         grantResults: IntArray
     ) {
         // Check if location permissions are granted
-        if (requestCode == REQUEST_FOREGROUND_PERMISSION_FOR_MY_LOCATION) {
-            if (grantResults.isNotEmpty() && hasAllPermissionGranted(grantResults)) {
-                navigateToSelectLocationFragment()
-            } else {
-                _viewModel.showSnackBarInt.value = R.string.permission_denied_explanation
+        when (requestCode) {
+            REQUEST_FOREGROUND_PERMISSION_FOR_MY_LOCATION -> {
+                if (grantResults.isNotEmpty() && hasAllPermissionGranted(grantResults)) {
+                    navigateToSelectLocationFragment()
+                } else {
+                    _viewModel.showSnackBarInt.value = R.string.permission_denied_explanation
+                }
+                return
             }
-        } else if (requestCode == REQUEST_FOREGROUND_PERMISSION || requestCode == REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSIONS) {
-            if (grantResults.isNotEmpty() && hasAllPermissionGranted(grantResults)) {
-                saveReminder()
+            REQUEST_BACKGROUND_PERMISSION_FOR_GEOFENCE -> {
+                if (grantResults.isNotEmpty() && hasAllPermissionGranted(grantResults)) {
+                    saveReminderAndStartGeofence()
+                }
+                return
+            }
+            else -> {
+
             }
         }
     }
@@ -113,21 +145,13 @@ class SaveReminderFragment : BaseFragment() {
     }
 
     @TargetApi(29)
-    private fun requestForegroundAndBackgroundPermissions() {
-        var permissionsArray = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
-
-        val resultCode = when {
-            runningQOrLater -> {
-                permissionsArray += Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSIONS
-            }
-            else -> REQUEST_FOREGROUND_PERMISSION
+    private fun requestBackgroundPermissionForGeofence() {
+        if (runningQOrLater) {
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                REQUEST_BACKGROUND_PERMISSION_FOR_GEOFENCE
+            )
         }
-
-        requestPermissions(
-            permissionsArray,
-            resultCode
-        )
     }
 
     private fun navigateToSelectLocationFragment() {
@@ -135,7 +159,7 @@ class SaveReminderFragment : BaseFragment() {
             NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToSelectLocationFragment())
     }
 
-    private fun saveReminder() {
+    private fun saveReminderAndStartGeofence() {
         val title = _viewModel.reminderTitle.value
         val description = _viewModel.reminderDescription.value
         val location = _viewModel.reminderSelectedLocationStr.value
@@ -146,17 +170,66 @@ class SaveReminderFragment : BaseFragment() {
         val reminderDataItem =
             ReminderDataItem(title, description, location, latitude, longitude)
         val isSuccess = _viewModel.validateAndSaveReminder(reminderDataItem)
+
+        // Start the geofence
         if (isSuccess) {
-            GeofenceUtils.addGeofence(requireContext(), reminderDataItem)
+            addGeofence(requireContext(), reminderDataItem)
         }
+    }
+
+    /**
+     * Shows rationale dialog for displaying why the app needs permission
+     */
+    private fun showRationaleDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(getString(R.string.geofence_permission_rationale_title))
+            .setMessage(getString(R.string.geofence_permission_rationale_message))
+            .setPositiveButton(getString(R.string.geofence_permission_rationale_ok)) { dialog, which ->
+                requestBackgroundPermissionForGeofence()
+            }
+        builder.create().show()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun addGeofence(context: Context, reminderDataItem: ReminderDataItem) {
+
+        val geofence = Geofence.Builder()
+            .setRequestId(reminderDataItem.id)
+            .setCircularRegion(
+                reminderDataItem.latitude!!.toDouble(),
+                reminderDataItem.longitude!!.toDouble(),
+                GeofencingConstants.GEOFENCE_RADIUS_IN_METERS
+            )
+            .setExpirationDuration(GeofencingConstants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+            .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+            .build()
+
+        val geofencingRequest = GeofencingRequest.Builder()
+            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            .addGeofence(geofence)
+            .build()
+
+        val geofencingClient = LocationServices.getGeofencingClient(context)
+
+
+        geofencingClient.addGeofences(
+            geofencingRequest,
+            geofencePendingIntent
+        )
+            .addOnSuccessListener {
+                Timber.d("Success! id: ${geofence.requestId}")
+            }
+            .addOnFailureListener {
+                if ((it.message != null)) {
+                    Timber.w(it.message!!)
+                }
+            }
     }
 
     companion object {
         private const val REQUEST_FOREGROUND_PERMISSION_FOR_MY_LOCATION = 32
-        private const val REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSIONS = 33
-        private const val REQUEST_FOREGROUND_PERMISSION = 34
+        private const val REQUEST_BACKGROUND_PERMISSION_FOR_GEOFENCE = 33
+
         private const val REQUEST_TURN_DEVICE_LOCATION_ON = 29
-
-
     }
 }
